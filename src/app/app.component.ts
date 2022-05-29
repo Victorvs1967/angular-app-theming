923bc4d63bb1acb0919e02cb4d29e16bb6a24da3
Victor <victorsmirnov@me.com>
2022-05-29 20:16:42 +0300

    create application

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Theme } from 'src/theme/theme';
+import { ThemeService } from 'src/theme/theme.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-app-theming';
+
+  private _themes = Object.values(Theme);
+
+  constructor(private _themingService: ThemeService) {}
+
+  public setPrevTheme(): void {
+    const current: number = this._themes.indexOf(this._themingService.currentTheme);
+    const next: Theme = this._themes[(current + this._themes.length + 1) % this._themes.length];
+    this._themingService.setTheme(next);
+  }
+
+  public setNextTheme(): void {
+    const current: number = this._themes.indexOf(this._themingService.currentTheme);
+    const previous: Theme = this._themes[(current + this._themes.length - 1) % this._themes.length];
+    this._themingService.setTheme(previous);
+  }
+
 }
